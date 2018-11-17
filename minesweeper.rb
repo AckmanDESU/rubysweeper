ab0335b96ff1b9f12a6a834c5eb253eee47db61f
AckmanDESU <torinoob@gmail.com>
2018-11-17 20:19:01 +0100

    Implement input validation methods

--- minesweeper.rb
+++ minesweeper.rb
@@ -11,19 +11,48 @@ class Minesweeper
     @board.over?
   end
 
+  def valid_pos?(pos)
+    begin
+      pos.map! {|el| Integer(el)}
+    rescue
+      return false
+    end
+
+    !@board[pos].nil?
+  end
+
+  def valid_mode?(mode)
+    ["r", "f", ""].include?(mode.downcase)
+  end
+
+  def valid_input?((row, col, mode))
+    valid_pos?([row, col]) && valid_mode?(mode)
+  end
+
+  def parse_input(input)
+    input
+      .split(/\s|,/)
+      .reject(&:empty?)
+  end
+
   def get_player_input
-    puts "(r)eveal or (f)lag a position: r|f,row,col - ex: r,5,3"
-    input = gets.chomp.split(",")
-    mode = input.shift
-    pos = input.map(&:to_i)
-    [mode, pos]
+    input = nil
+
+    until input && valid_input?(input)
+      puts "Enter a position like so: row, column - add an 'f' at the end to flag it or 'r' to reveal it"
+      print "> "
+      input = parse_input(gets.chomp)
+    end
+
+    input
   end
 
   def play
     until game_over?
       @board.render
 
-      mode, pos = get_player_input
+      *pos, mode = get_player_input
+      pos.map!(&:to_i)
 
       puts "mode:  #{mode}"
       puts "pos:  #{pos}"
